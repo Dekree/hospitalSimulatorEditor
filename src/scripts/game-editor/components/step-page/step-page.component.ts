6c2176da472032b0ccd26c9843c235461991b6d7
Andrey Viriyalov <aviriyalov@gmail.com>
2017-06-26 17:36:00 +0500

    step-page #progress

--- src/scripts/game-editor/components/step-page/step-page.component.ts
+++ src/scripts/game-editor/components/step-page/step-page.component.ts
@@ -1,4 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
+import { NotificationsService } from 'angular2-notifications/dist';
+
+import { GameDataService } from '../../services/game-data/game-data.service';
+import { GameLoaderService } from 'game-loader/services';
+
+import { IStepMetadata, IQuestParam, IQuestMetadata, IQuestList } from '../../interfaces';
 
 @Component( {
     selector: 'step-page',
@@ -7,10 +16,49 @@ import { Component, ViewEncapsulation } from '@angular/core';
     styleUrls: [ './step-page.style.less' ]
 } )
 
-export class StepPageComponent {
+export class StepPageComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    private questData: any;
 
+    private questName: string = '';
+    private rubricaId: string;
+    private questId: string;
+
+    private steps: IStepMetadata[] = [];
+    private routeParams: Subscription;
+
+    constructor( private gameDataService: GameDataService,
+                 private notificationsService: NotificationsService,
+                 private loaderService: GameLoaderService,
+                 private router: Router,
+                 private route: ActivatedRoute ) {
+    }
+
+    private getQuestMetadata( questNumber: string ): void {
+        let questParam: IQuestParam = {
+            number: questNumber,
+            name: ''
+        };
+
+        this.gameDataService.getQuests( [ questParam ] )
+            .then( ( questMetadata: IQuestList ) => {
+
+            } );
+    }
+
+    ngOnInit() {
+        this.loaderService.show( 'Идет загрузка данных...' );
+
+        this.routeParams = this.route.params.subscribe( ( params ) => {
+            this.questId = params[ 'questId' ];
+            this.rubricaId = params[ 'rubricaId' ];
+
+            this.getQuestMetadata( this.questId );
+        } );
+    }
+
+    ngOnDestroy() {
+        this.routeParams.unsubscribe();
     }
 
 }
